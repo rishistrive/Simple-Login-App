c27237a8b15501ffb0b2dfc381e73a654d76e51c
rishistrive <rishi.strive@gmail.com>
2021-11-24 21:20:13 +0530

    Work for heroku deployment

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const path = require("path");
 require("../src/db/conn");
 const app = express();
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 3000;
 var cors = require("cors");
 const routerUsers = require("./routers/usersRouter");
 app.use(cors());
 app.use(express.json());
 app.use(routerUsers);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`connection is live at port ${port}`);
--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -5,8 +5,7 @@ import rootReducer from "./Reducers";
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 export default store;
